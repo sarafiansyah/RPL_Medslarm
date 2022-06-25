ad886b2eabe197064dd71f05d1cd002193a8c664
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2022-06-25 16:00:06 +0700

    Update Calendar

--- fw/footer-main.php
+++ fw/footer-main.php
@@ -39,12 +39,12 @@
     <!-- Section: Social media -->
 
     <!-- Section: Links  -->
-    <section class="">
+    <section class="" >
       <div class="container text-center text-md-start mt-5">
         <!-- Grid row -->
         <div class="row mt-3">
           <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4" style="text-align: left;">
             <!-- Content -->
             <h6 class="text-uppercase fw-bold">Company name</h6>
             <hr
@@ -60,7 +60,7 @@
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4" style="text-align: left;">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold">Products</h6>
             <hr
@@ -83,7 +83,7 @@
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4" style="text-align: left;">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold">Useful links</h6>
             <hr
@@ -106,7 +106,7 @@
           <!-- Grid column -->
 
           <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4" style="text-align: left;">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold">Contact</h6>
             <hr

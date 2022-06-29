94af3654a8d93a6bb99d1f935bdb7ec6636abb0a
Mahesa Rafian Syah <mahesarafiansyah@gmail.com>
2022-06-29 12:29:00 +0700

    Update public-signUp.php

--- public-signUp.php
+++ public-signUp.php
@@ -1,3 +1,4 @@
+
 <?php
 //REG
 require '_php/function.php';
@@ -8,16 +9,7 @@ if (isset($_SESSION['username'])) {
    header("Location: index.php");
 }
 
-if(isset($_POST['register'])) {
-  if(registration($_POST) > 0){
-    echo "<script>
-          alert('User baru berhasil');
-          </script>";
-  }
-}
-else {
-  echo mysqli_error($conn);
-}
+
 
 
 //LOG
@@ -65,9 +57,20 @@ if(isset($_POST['loginBtn'])) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
   </head>
   <body>
+    <?php if(isset($_POST['register'])) {
+  if(registration($_POST) > 0){
+    echo "<script>
+    swal(\"Good job!\", \"You clicked the button!\", \"success\");
+          </script>";
+  }
+}
+else {
+  echo mysqli_error($conn);
+} ?>
   <div class="mainbg1">
 <?php  include("fw/nav-main.php") ?>
 </div>
